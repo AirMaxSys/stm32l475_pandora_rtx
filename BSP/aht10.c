b81a510ec871aa278946c1422a307290cea33b69
AirMaxSys <gy520813@live.com>
2021-03-09 22:49:55 +0800

    complete aht10 sensor

--- BSP/aht10.c
+++ BSP/aht10.c
@@ -1,5 +1,6 @@
 #include "aht10.h"
-#include <stdio.h>
+
+#define AHT10_DBG	1
 
 #define AHT10_CMD_CALIBRATE	0xE1u
 #define AHT10_CMD_FETCH_DAT	0xACu
@@ -36,30 +37,34 @@ void aht10_init(aht10_t *paht)
 	i2c_soft_init(&aht10_i2c_dev);
 	printf("AHT10 init ok, status value is:0x%02x\r\n", aht10_status_reg());
 	paht->temp = paht->humi = 0;
+	memset(paht->buf, 0x0, AHT10_BUFFER_SIZE);
 	aht10_calibrate();
 }
 
-void aht10_get_value(void)
+void aht10_get_value(aht10_t *paht)
 {
-	uint8_t data[6];
 	uint8_t cmd[] = {AHT10_CMD_FETCH_DAT, 0, 0};
 	i2c_soft_write(cmd, 3);
-	i2c_soft_read(data, 6);
+	i2c_soft_read(paht->buf, 6);
+#ifdef AHT10_DBG
 	printf("AHT10 fetch row data:");
-	for (uint8_t i = 0; i < 6; ++i)
-		printf("0x%02x ", data[i]);
+	for (uint8_t i = 0; i < AHT10_BUFFER_SIZE; ++i)
+		printf("0x%02x ", paht->buf[i]);
 	puts("\r");
-	float humi = ((data[1]) << 12 | data[2] << 4 | (data[3] & 0xF0)) * 100.0 / (1 << 20);
-	float temp = ((data[3] & 0xf) << 16 | data[4] << 8 | data[5]) * 200.0 / (1 << 20) - 50;
-	printf("AHT10 temp:%.1f humi:%.1f%%\r\n", temp, humi);
+#endif
+	paht->humi = ((paht->buf[1]) << 12 | paht->buf[2] << 4 | (paht->buf[3] & 0xF0)) * 1000.0 / (1 << 20);
+	paht->temp = ((paht->buf[3] & 0xf) << 16 | paht->buf[4] << 8 | paht->buf[5]) * 2000.0 / (1 << 20);
+#ifdef AHT10_DBG
+	printf("AHT10 temp:%.1f humi:%.1f%%\r\n", aht10_temp(paht), aht10_humi(paht));
+#endif
 }
 
-float aht10_temp(void)
+float aht10_temp(aht10_t *paht)
 {
-
+	return (paht->temp/10.0-50);
 }
 
-float ath10_humi(void)
+float aht10_humi(aht10_t *paht)
 {
-
+	return (paht->humi/10.0);
 }
--- BSP/aht10.h
+++ BSP/aht10.h
@@ -7,17 +7,22 @@ extern "C" {
 
 #include "i2c_soft.h"
 #include "main.h"
+#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+
+#define AHT10_BUFFER_SIZE	6u
 
 typedef struct aht10 {
 	uint16_t temp;
 	uint16_t humi;
+	uint8_t buf[AHT10_BUFFER_SIZE];
 } aht10_t;
 
 void aht10_init(aht10_t *paht);
-void aht10_get_value(void);
-float aht10_temp(void);
-float ath10_humi(void);
+void aht10_get_value(aht10_t *paht);
+float aht10_temp(aht10_t *paht);
+float aht10_humi(aht10_t *paht);
 
 uint8_t aht10_status_reg(void);
 
